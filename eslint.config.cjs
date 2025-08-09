d22bef73354889a7a9abaf405f0dfb7fb2adf55f
Michael Crawford <cerealean@users.noreply.github.com>
2025-08-09 04:24:39 -0500

    chore: updated eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,49 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module",
+        parserOptions: {},
+
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+            ...globals.worker,
+        },
+    },
+
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "prefer-const": "error",
+        "no-var": "error",
+    },
+}]);
